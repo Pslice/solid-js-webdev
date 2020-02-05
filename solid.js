6da5f52326717258bf3e9c08e714e82b731695ac
Patrick Cool <patrickscool@gmail.com>
2020-02-04 21:31:52 -0800

    added Store class

--- solid.js
+++ solid.js
@@ -1,3 +1,17 @@
 //single responsibility
 //
 //dependency inversion
+class Store {
+    constructor(user) {
+        this.stripe = new this.stripe(user);
+    }
+
+    purchaseTubing(qty) {
+        this.stripe.makePayment();
+    }
+    purchaseSensors(qty) {
+        this.stripe.makePayment();
+    }
+
+}
+
