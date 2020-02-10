2f15f10066774a25a17f6385a5d1ce078dcc84c7
Patrick Cool <patrickscool@gmail.com>
2020-02-09 22:15:34 -0800

    fake Stripe class

--- solid.js
+++ solid.js
@@ -7,9 +7,18 @@ class Store {
     }
 
     purchaseTubing(qty) {
-        this.stripe.makePayment();
+        this.stripe.makePayment(200 * qty * 100);
     }
     purchaseSensors(qty) {
-        this.stripe.makePayment();
+        this.stripe.makePayment(15 * qty);
+    }
+}
+
+class Stripe {
+    constructor(user) {
+        this.user = user
+    }
+    makePayment(amount) {
+        console.log(`${this.user} made payment of ${amount / 100} with Stripe`)
     }
 }
